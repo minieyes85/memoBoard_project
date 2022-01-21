1da117c8226c3e1b4da78e183d35607d9a3a2bf8
JSChoi <minieyes85@gmail.com>
2022-01-21 20:28:09 +0900

    220121 first commit

---
+++ src/main/java/com/minieyes/memo/MemoBoardApplication.java
@@ -0,0 +1,13 @@
+package com.minieyes.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minieyes/memo/MemoBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minieyes.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
