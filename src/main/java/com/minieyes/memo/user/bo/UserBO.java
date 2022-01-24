8c8a55ada012f2a9b836cda8ef5205a0cce3c49f
JSChoi <minieyes85@gmail.com>
2022-01-24 21:48:41 +0900

    220124 password 암호화 기능 추가, md5 type?

--- src/main/java/com/minieyes/memo/user/bo/UserBO.java
+++ src/main/java/com/minieyes/memo/user/bo/UserBO.java
@@ -3,6 +3,7 @@ package com.minieyes.memo.user.bo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.minieyes.memo.common.EncryptUtils;
 import com.minieyes.memo.user.dao.UserDAO;
 
 @Service
@@ -12,7 +13,11 @@ public class UserBO {
 	private UserDAO userDAO;
 	
 	public int addUser(String loginId, String password, String name, String email) {
-		return userDAO.insertUser(loginId, password, name, email);
+		
+		//암호화
+		String encodingPassword = EncryptUtils.md5(password);
+		
+		return userDAO.insertUser(loginId, encodingPassword, name, email);
 	}
 
 }
